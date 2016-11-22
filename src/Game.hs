2d06dbf92ffb655dbada16317af95b3727f2f534
dgrisham <dgrisham@mines.edu>
2016-11-22 10:24:01 -0700

    big refactor comin'

--- src/Game.hs
+++ src/Game.hs
@@ -17,5 +17,13 @@ module Game where
 
 import qualified Utils.Censor as Censor
 import qualified Utils.AS as AS
+import Utils.Types.Route (Routes)
 
+
+--------------------------------------------------------------------------------
+-- # AS Subgame: Best Response Dynamics
+--------------------------------------------------------------------------------
+
+bestResponseDynamics :: Routes -> AS.Players -> AS.Players
+bestResponseDynamcis routes players =
 
